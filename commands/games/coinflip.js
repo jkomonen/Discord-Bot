03de1a4164ba4416f2047f36718e8951ae632382
jkomonen <68486286+jkomonen@users.noreply.github.com>
2021-07-23 08:14:19 -0400

    readme, diceroll and coinflip tweaks

--- commands/games/coinflip.js
+++ commands/games/coinflip.js
@@ -4,14 +4,8 @@ module.exports = {
     enable: true,
 
     async execute(discord, client, args, cmd, message, prefix) {
-        const choices = ['heads', 'tails'];
+        const choices = ['https://i.imgur.com/28BSgRT.png', 'https://i.imgur.com/fdto2mP.png'];
         const choice = choices[Math.floor(Math.random() * choices.length)];
-        const embed = new discord.MessageEmbed()
-            .setTitle('Coinflip')
-            .setColor('#4287f5')
-            .setDescription(`You flipped ${choice}`)
-            .setTimestamp()
-            .setFooter('This command was made by Buzz Bot')
-        message.channel.send(embed);
+        message.channel.send(choice);
     }
 }
--- commands/games/diceroll.js
+++ commands/games/diceroll.js
@@ -4,14 +4,15 @@ module.exports = {
     enable: true,
 
     execute(discord, client, args, cmd, message, prefix) {
-        const choices = ['1', '2', '3', '4', '5', '6'];
+        const choices = ['https://i.imgur.com/uAecgop.png', 'https://i.imgur.com/JaDMqMC.png', 'https://i.imgur.com/tOXMmU3.png', 'https://i.imgur.com/BS5Sr9D.png', 'https://i.imgur.com/QSZg2i8.png', 'https://i.imgur.com/NRFMfGz.png'];
         const choice = choices[Math.floor(Math.random() * choices.length)];
-        const embed = new discord.MessageEmbed()
-            .setTitle('Diceroll')
-            .setColor('#4287f5')
-            .setDescription(`You rolled a ${choice}`)
-            .setTimestamp()
-            .setFooter('This command was made by Buzz Bot')
-        message.channel.send(embed);
+        // const embed = new discord.MessageEmbed()
+        //     .setTitle('Diceroll')
+        //     .setColor('#4287f5')
+        //     .setDescription(`You rolled a ${choice}`)
+        //     .setTimestamp()
+        //     .setFooter('This command was made by Buzz Bot')
+        // message.channel.send(embed);
+        message.channel.send(choice);
     }
 }
